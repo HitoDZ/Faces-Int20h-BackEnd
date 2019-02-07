7a933655ecf1fbfeee0b006fedf4d58f5c977fbc
Vova Lantsov <vova.lantsov.25.private@gmail.com>
2019-02-07 14:44:33 +0200

    Added options for ClickrClient class.

---
+++ Api.Flickr/FlickrClientOptions.cs
@@ -0,0 +1,9 @@
+namespace Api.Flickr
+{
+    public sealed class FlickrClientOptions
+    {
+        public string UserId;
+        public string ApiKey;
+        public string PhotosetId;
+    }
+}
