60814f78458dfa86edc530f4af5e7d8fc92f52e0
Vova Lantsov <vova.lantsov.25.private@gmail.com>
2019-02-07 22:10:34 +0200

    Added response statuses for face++ result of detect API

---
+++ Api.FacePlusPlus/Models/FacePlusPlusDetectResponseStatus.cs
@@ -0,0 +1,13 @@
+namespace Api.FacePlusPlus.Models
+{
+    public enum FacePlusPlusDetectResponseStatus
+    {
+        AuthenticationError,
+        AuthorizationError,
+        ConcurrencyLimitExceeded,
+        BadArguments,
+        ApiNotFound,
+        InternalError,
+        AnotherError
+    }
+}
