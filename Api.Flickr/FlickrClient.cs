ee7157e9bd018a0340bf38e41521c495d632b7de
Vova Lantsov <vova.lantsov.25.private@gmail.com>
2019-02-07 16:04:13 +0200

    Added response json handler using dynamic calls

--- Api.Flickr/FlickrClient.cs
+++ Api.Flickr/FlickrClient.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Api.Flickr.Abstractions;
 using Api.Flickr.Models;
+using Newtonsoft.Json;
 
 namespace Api.Flickr
 {
@@ -31,7 +32,17 @@ namespace Api.Flickr
                 if (response == null)
                     return new FlickrPhotosetsGetPhotosResponse(0, "Response is null");
 
-                return null;
+                dynamic json = JsonConvert.DeserializeObject(response);
+                
+                switch (json.stat)
+                {
+                    case "fail": return new FlickrPhotosetsGetPhotosResponse(json.code, json.message);
+                    
+                    case "ok":
+                        return new FlickrPhotosetsGetPhotosResponse(json.photoset.photo);
+
+                    default: return new FlickrPhotosetsGetPhotosResponse(0, "stat is incorrect");
+                }
             }
             catch (Exception e)
             {
