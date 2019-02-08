dfee7380f14346f9edf84556dd08747a52ba05cc
Vova Lantsov <vova.lantsov.25.private@gmail.com>
2019-02-08 13:33:45 +0200

    Added basic Face++ client that implements IFacePlusPlusClient interface

---
+++ Api.FacePlusPlus/FacePlusPlusClient.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Api.FacePlusPlus.Abstractions;
+using Api.FacePlusPlus.Models;
+
+namespace Api.FacePlusPlus
+{
+    public sealed class FacePlusPlusClient : IFacePlusPlusClient, IDisposable
+    {
+        private readonly HttpClient _client = new HttpClient
+        {
+            BaseAddress = new Uri("https://api-us.faceplusplus.com/facepp/v3/"),
+            Timeout = TimeSpan.FromSeconds(10d)
+        };
+
+        private readonly FacePlusPlusClientOptions _options;
+
+        public FacePlusPlusClient(FacePlusPlusClientOptions options) => _options = options;
+
+        public async Task<List<FacePlusPlusEmotionResult>> GetEmotionsForPhoto(string photoUrl)
+        {
+            var requestUri = $"detect?api_key={_options.ApiKey}&api_secret={_options.ApiSecret}&image_url={photoUrl}&return_attributes=emotion";
+            var response = await _client.GetAsync(requestUri);
+
+            return null;
+        }
+
+        public void Dispose() => _client.Dispose();
+    }
+}
