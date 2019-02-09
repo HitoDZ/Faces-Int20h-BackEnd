3ccf064928031722cd08e2509aee45ff43f3d701
Vova Lantsov <vova.lantsov.25.private@gmail.com>
2019-02-09 20:21:45 +0200

    Added base migration logic (receiving photos)

---
+++ Mongo/Mongo.Migrations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Api.Flickr;
+using Api.Flickr.Models;
+
+namespace Mongo.Migrations
+{
+    internal class Program
+    {
+        private static readonly FlickrClientOptions _flickrClientOptions = new FlickrClientOptions
+        {
+            ApiKey = Defaults.FLICKR_API_KEY,
+            UserId = "118310678@N03",
+            PhotosetId = "72157641322954544"
+        };
+        
+        internal static async Task Main(string[] args)
+        {
+            FlickrPhotosetsGetPhotosResult flickrResult;
+            
+            using (var flickrClient = new FlickrClient(_flickrClientOptions))
+                flickrResult = await flickrClient.GetPhotosAsync();
+
+            if (!flickrResult.IsOk)
+            {
+                Error("An error occurred while getting the list of photos from Flickr API");
+                Error(flickrResult.ErrorMessage);
+                return;
+            }
+            
+            Info("Successfully received the list of photos from Flickr API");
+            Info($"Count of photos: {flickrResult.Photos.Count}");
+        }
+
+        private static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        private static void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
