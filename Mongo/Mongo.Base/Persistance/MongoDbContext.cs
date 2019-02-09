f3c9f0ac4883c3da0240bca0d15d9d4363f5e6ba
Zanora <hitodz97@gmail.com>
2019-02-09 19:58:30 +0200

    Fix models and sln locations

--- Mongo/Mongo.Base/Persistance/MongoDbContext.cs
+++ Mongo/Mongo.Base/Persistance/MongoDbContext.cs
@@ -3,14 +3,14 @@ using MongoDB.Driver;
 
 namespace Mongo.Base
 {
-    public sealed class PhotosContext
+    public sealed class MongoDbContext
     {
         public readonly IMongoCollection<PhotoModel> Photos;
         
-        public PhotosContext(string connectionString)
+        public MongoDbContext(string connectionString)
         {
             var mongoClient = new MongoClient(connectionString);
-            var db = mongoClient.GetDatabase(nameof(PhotosContext));
+            var db = mongoClient.GetDatabase(nameof(MongoDbContext));
 
             Photos = db.GetCollection<PhotoModel>(nameof(Photos));
         }
